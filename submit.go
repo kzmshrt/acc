55f581f27312559242bf44b679b2c8ec18ae639d
akaimononoke <p.m.byzance0817@gmail.com>
2020-09-04 00:21:34 +0900

    2020-09-04 00:21:34

--- submit.go
+++ submit.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/sclevine/agouti"
@@ -72,7 +73,12 @@ func newSeleniumSubmitter() *seleniumSubmitter {
 }
 
 func (*seleniumSubmitter) Submit(taskUrl string, sourceCode []byte) (*submitResult, error) {
-	driver := agouti.ChromeDriver()
+	// driver
+	driver := agouti.ChromeDriver(
+		agouti.ChromeOptions("args", []string{
+			"--headless",
+		}),
+	)
 	err := driver.Start()
 	if err != nil {
 
@@ -80,6 +86,7 @@ func (*seleniumSubmitter) Submit(taskUrl string, sourceCode []byte) (*submitResu
 	}
 	defer driver.Stop()
 
+	// page
 	page, err := driver.NewPage()
 	if err != nil {
 		return nil, err
@@ -103,9 +110,66 @@ func (*seleniumSubmitter) Submit(taskUrl string, sourceCode []byte) (*submitResu
 	page.RunScript("$('.editor').data('editor').setValue(sourceCode)", map[string]interface{}{"sourceCode": string(sourceCode)}, nil)
 	page.FindByID("submit").Click()
 
-	time.Sleep(time.Second * 20)
+	// submissions
+	time.Sleep(2 * time.Second)
+	newSubmission := page.Find("table").Find("tbody").First("tr")
+	cols := newSubmission.All("td")
+	ncol, err := cols.Count()
+	if err != nil {
+		return nil, err
+	}
+	lastElem := cols.At(ncol - 1)
+	detailUrl, err := lastElem.Find("a").Attribute("href")
+	if err != nil {
+		return nil, err
+	}
+
+	// submission detail
+	err = page.Navigate(detailUrl)
+	if err != nil {
+		return nil, err
+	}
+	for isLabelDefault(page) {
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	tables := page.AllByClass("table-bordered")
+
+	codeLength := tables.At(0).Find("tbody").All("tr").At(5)
+	codeLengthTitle, _ := codeLength.Find("th").Text()
+	codeLengthTitle = strings.TrimSpace(codeLengthTitle)
+	codeLengthValue, _ := codeLength.Find("td").Text()
+	codeLengthValue = strings.TrimSpace(codeLengthValue)
+
+	resultStatus := tables.At(0).Find("tbody").All("tr").At(6)
+	resultStatusTitle, _ := resultStatus.Find("th").Text()
+	resultStatusTitle = strings.TrimSpace(resultStatusTitle)
+	resultStatusValue, _ := resultStatus.Find("td").Text()
+	resultStatusValue = strings.TrimSpace(resultStatusValue)
+
+	timeScore := tables.At(0).Find("tbody").All("tr").At(7)
+	timeScoreTitle, _ := timeScore.Find("th").Text()
+	timeScoreTitle = strings.TrimSpace(timeScoreTitle)
+	timeScoreValue, _ := timeScore.Find("td").Text()
+	timeScoreValue = strings.TrimSpace(timeScoreValue)
+
+	memoryScore := tables.At(0).Find("tbody").All("tr").At(8)
+	memoryScoreTitle, _ := memoryScore.Find("th").Text()
+	memoryScoreTitle = strings.TrimSpace(memoryScoreTitle)
+	memoryScoreValue, _ := memoryScore.Find("td").Text()
+	memoryScoreValue = strings.TrimSpace(memoryScoreValue)
+
+	fmt.Println(resultStatusTitle, ":", resultStatusValue)
+	fmt.Println(timeScoreTitle, ":", timeScoreValue)
+	fmt.Println(memoryScoreTitle, ":", memoryScoreValue)
+	fmt.Println(codeLengthTitle, ":", codeLengthValue)
 
 	return nil, nil
+}
+
+func isLabelDefault(page *agouti.Page) bool {
+	classAttribute, _ := page.FindByID("judge-status").Find("span").Attribute("class")
+	return strings.Contains(classAttribute, "label-default")
 }
 
 func Submit(url, filename string) (*submitResult, error) {
